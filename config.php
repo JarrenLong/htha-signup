29fe9a7d5f4b031dd3fbbe51865214b086ba8bd1
Jarren Long <jarrenlong@gmail.com>
2024-09-20 08:52:48 -0700

    Prepping for publish

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 // 
 // Don't change anything below this line!!!
 // 
-$servername = "mysql.helpingtruckers.all2a.net";
-$username = "hthahelp";
-$password = "y9j#AmST";
-$dbname = "htha_helpform";
+$servername = "";
+$username = "";
+$password = "";
+$dbname = "";
 ?>
